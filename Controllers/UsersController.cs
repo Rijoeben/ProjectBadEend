45735412f0f877b8996b923e19988e5ffb310339
unknown <alexander.noez@hotmail.com>
2021-10-15 16:19:47 +0200

    first commit

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bad_eend
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private IBadeendDataContext _data;
+
+        public UsersController(IBadeendDataContext data)
+        {
+            _data = data;
+        }
+        [HttpGet]
+        public ActionResult<IEnumerable<Users>> Get()
+        {
+            return Ok(_data.GetUsers());
+        }
+        [HttpGet("{user_id}")]
+        public ActionResult<IEnumerable<Users>> GetUser(int user_id)
+        {
+            return Ok(_data.GetUser(user_id));
+        }
+
+
+
+        [HttpPost]
+        public ActionResult<Users> Post([FromBody] Users user)
+        {
+            _data.AddUser(user);
+            return Ok("Gg well played");
+        }
+    }
+}
---
+++ Models/Posts.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bad_eend
+{
+    public class Posts
+    {
+        public int Id { get; private set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime Post_Date { get; private set; }
+        public int Likes { get; set; }
+        public int User_Id { get; set; }
+    }
+}
