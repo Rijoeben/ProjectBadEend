914c9537f74a853fc1ceb2491017ce2a3b77c23f
unknown <alexander.noez@hotmail.com>
2021-10-20 14:27:36 +0200

    initial commit

---
+++ Controllers/FollowersController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestMongoDB.Services;
+using TestMongoDB.Models;
+
+namespace TestMongoDB.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FollowersController : ControllerBase
+    {
+        private readonly FollowersService _followerService;
+
+        public FollowersController(FollowersService followersService)
+        {
+            _followerService = followersService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Followers>> Get() =>
+            _followerService.Get();
+
+        [HttpGet("{id:length(24)}", Name = "GetFollower")]
+        public ActionResult<Followers> Get(string id)
+        {
+            var book = _followerService.Get(id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return book;
+        }
+
+        [HttpPost]
+        public ActionResult<Followers> Create(string userId)
+        {
+            _followerService.Create(userId);
+
+            return CreatedAtRoute("GetFollower", new { id = userId.ToString() });
+        }
+
+        [HttpPut("{id:length(24)}")]
+        public IActionResult Update(string id, string follower)
+        {
+            var user = _followerService.Get(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            _followerService.Update(id, follower);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:length(24)}")]
+        public IActionResult Delete(string id)
+        {
+            var follower = _followerService.Get(id);
+
+            if (follower == null)
+            {
+                return NotFound();
+            }
+
+            _followerService.Remove(follower.Id);
+
+            return NoContent();
+        }
+    }
+}
---
+++ Models/FollowersDatabaseSettings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestMongoDB.Models
+{
+    public class FollowersDatabaseSettings : IFollowersDatabaseSettings
+    {
+        public string FollowersCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+
+    public interface IFollowersDatabaseSettings
+    {
+        string FollowersCollectionName { get; set; }
+        string ConnectionString { get; set; }
+        string DatabaseName { get; set; }
+    }
+}
