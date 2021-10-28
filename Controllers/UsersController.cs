aab7a941c9188c338f9b793814cd56a928158bc0
Rijoeben <61225406+Rijoeben@users.noreply.github.com>
2021-10-28 13:27:09 +0200

    Revert "After merge"
    This reverts commit 42f68226e26f7969e3607a478df2fdf16847c558.

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -41,7 +41,9 @@ namespace Bad_eend
             _data.UpdateLastPosted(user_id, DateTime.Now);
             return Ok("Record updated");
         }
-                     
+
+
+      
 
         [HttpDelete]
         public ActionResult<Users> DeleteUser(int user_id)
