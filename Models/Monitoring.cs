e810f62d7a3050f72dd4ddded2e1b8b040fac0d3
Rijoeben <61225406+Rijoeben@users.noreply.github.com>
2021-12-03 16:30:16 +0100

    First Commit

---
+++ Models/Monitoring.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bad_eend
+{
+    public class Monitoring
+    {
+        public int Id { get; private set; }
+        public DateTime Date { get; set; }
+        public string Type { get; set; }
+        public int ErrorCode { get; set; }
+        // public string Ip {get;set;} "optioneel als het mogelijk is"
+    }
+}
